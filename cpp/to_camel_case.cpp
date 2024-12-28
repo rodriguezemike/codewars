74b8bd6957fe1f89015db21f41bb54e394e6b25d
Michael Rodriguez <rodriguez.e.mike@gmail.com>
2024-12-28 07:15:19 -0800

    solution synthesis

--- cpp/to_camel_case.cpp
+++ cpp/to_camel_case.cpp
@@ -1,8 +1,7 @@
 std::string to_camel_case(std::string text) {
-  std::set<char> delimiters = {'-', '_'};
   std::string result = "";
   for (int i=0; i < int(text.size()); ++i){
-    if(delimiters.find(text[i]) != delimiters.end()){
+    if(text[i] == '-' || text[i] == '_'){
       result = result + std::string(1, toupper(text[i+1]));
       ++i;
     } else {
@@ -10,4 +9,16 @@ std::string to_camel_case(std::string text) {
     }
   }
   return result;
+}
+
+std::string to_camel_case_erase(std::string text){
+  for(int i = 0; i < text.size(); ++i)
+  {
+    if(text[i] == '-' || text[i] == '_')
+    {
+      text.erase(i,1);
+      text[i] = toupper(text[i]);
+    }
+  }
+  return text;
 }
